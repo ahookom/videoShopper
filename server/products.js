1c9dc0e53f60a27d290f3c36661d96de4bbf6288
Bruce Grugett <bgrugett@gmail.com>
2017-04-18 15:43:28 -0400

    product 2nd time

---
+++ server/products.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const {Product} = require('APP/db')
+
+const {mustBeLoggedIn, forbidden} = require('./auth.filters')
+
+module.exports = require('express').Router()
+  .get('/',
+    (req, res, next) =>
+      Product.findAll()
+        .then(products => res.json(products))
+        .catch(next))
+  .post('/',
+    (req, res, next) =>
+      Product.create(req.body)
+      .then(product => res.status(201).json(user))
+      .catch(next))
+  .param("id", (req, res, next, id) => {
+    Product.findById(id)
+      .then((foundProduct) => {
+        if (!foundProduct) {
+          res.sendStatus(404);
+        } else {
+          req.product = foundProduct;
+          next();
+        }
+      })
+      .catch(next);
+  })
+  .get('/:id',
+    (req, res, next) =>
+      res.send(req.product))
+  .put('/:id',
+    mustBeLoggedIn,
+    //check if admin
+    (req, res, next) => {
+     foundProduct.update (req.body)
+       .then((updatedProduct) => res.json(updatedProduct))
+       .catch(next)
+    })
+    .delete('/:id', (req, res, next) => {
+      foundProduct.update({isActive: false})
+      .then(res.sendStatus(204))
+    })
