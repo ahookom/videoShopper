977280df5c6a06556711723f0e80357bbe52aa31
Bruce Grugett <bgrugett@gmail.com>
2017-04-20 12:46:57 -0400

    userReducer#46

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux'
+import authReducer from './auth'
+import userReducer from './user'
+import productReducer from './product'
+import orderReducer from './order'
 
 const rootReducer = combineReducers({
-  auth: require('./auth').default,
+  auth: authReducer,
+  users: userReducer,
+  products: productReducer,
+  orders: orderReducer
 })
 
 export default rootReducer
---
+++ app/reducers/order.js
@@ -0,0 +1,56 @@
+// Required libraries
+import axios from 'axios'
+
+// ----------- Actions
+const FETCH_ORDERS = 'FETCH_ORDERS'
+
+// ----------- Action Creators
+export const receiveOrders = (orders) => ({
+  type: FETCH_ORDERS,
+  orders
+})
+
+// ----------- Reducer
+const initialState = {
+  allOrders: []
+}
+
+export default function orderReducer(state = initialState, action) {
+  const nextState = Object.assign({}, state)
+
+  switch (action.type) {
+  case FETCH_ORDERS:
+    nextState.allOrders = action.orders
+    break
+
+  default:
+    return state
+  }
+  return nextState
+}
+
+// ----------- Disptachers
+export const fetchOrders = () => (dispatch) => {
+  axios.get('/api/orders')
+      .then(response => {
+        dispatch(receiveOrders(response.data))
+      })
+      .catch(console.error)
+}
+
+export const addOrder = (orderData) => (dispatch) => {
+  axios.post('/api/orders/add', orderData)
+      .then(() => {
+        // dispatch(newOrder(response.data));
+        dispatch(fetchOrders())
+      })
+      .catch(console.error)
+}
+
+export const removeOrder = (orderId) => (dispatch) => {
+  axios.delete(`/api/orders/delete/${orderId}`)
+      .then(() => {
+        dispatch(fetchOrders())
+      })
+      .catch(console.error)
+}
