cdb94eaea4251e05fa60007ba79e44771e9af6d9
Andrew Hookom <hookom@gmail.com>
2017-04-20 12:30:20 -0400

    frontendroutes are up and working with dummy views

--- app/main.jsx
+++ app/main.jsx
@@ -9,8 +9,15 @@ import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import NotFound from './components/NotFound'
-import Home from './components/Home'
 
+import App from './components/App'
+
+import HomeView from './components/HomeView'
+import AdminView from './components/AdminView'
+import CartView from './components/CartView'
+import ProductsView from './components/ProductsView'
+import ProductView from './components/ProductView'
+import UserAccountView from './components/UserAccountView'
 
 //add to sign up/sign in component
 const ExampleApp = connect(
@@ -29,8 +36,14 @@ const ExampleApp = connect(
 render(
  <Provider store={store}>
    <Router history={browserHistory}>
-     <Route path="/" component={Home} >
-          <IndexRedirect to="/" />
+     <Route path="/" component={App} >
+       <Route path='/home' component={HomeView} />
+       <Route path='/products' component={ProductsView} />
+       <Route path='/product/:id' component={ProductView} />
+       <Route path='/user/:id' component={UserAccountView} />
+       <Route path='/cart' component={CartView} />
+       <Route path='/admin' component={AdminView} />
+       <IndexRedirect to="/home" />
      </Route>
      <Route path='*' component={NotFound} />
 
