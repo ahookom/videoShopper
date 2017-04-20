66913705b8fc7a02cadeaa939cdf2da08e61a55e
mandi-m <mmeidlinger@yahoo.com>
2017-04-20 11:45:56 -0400

    updated review to include logged in user

--- server/reviews.js
+++ server/reviews.js
@@ -2,21 +2,9 @@
 
 const {Review} = require('APP/db')
 
-const {mustBeLoggedIn, forbidden} = require('./auth.filters')
+const {mustBeLoggedIn, forbidden, mustBeAdminOrSelf} = require('./auth.filters')
 
 module.exports = require('express').Router()
-  .get('/',
-    (req, res, next) =>
-      Review.findAll()
-        .then(allReviews => res.json(allReviews))
-        .catch(next))
-  .post('/',
-    (req, res, next) => {
-      console.log('made it to reviews post with body of ', req.body)
-      Review.create(req.body)
-      .then(review => res.status(201).json(review))
-      .catch(next)
-    })
   .param('id', (req, res, next, id) => {
     Review.findById(id)
       .then((foundReview) => {
@@ -29,6 +17,27 @@ module.exports = require('express').Router()
       })
       .catch(next)
   })
+  .get('/',
+    (req, res, next) =>
+      Review.findAll()
+        .then(allReviews => res.json(allReviews))
+        .catch(next))
+  .post('/product/:productId/',
+    mustBeLoggedIn,
+    (req, res, next) => {
+      //console.log('made it to reviews post with body of ', req.body)
+      let review
+      Review.create(req.body)
+      .then(createdReview => {
+        review = createdReview
+        return review.setProduct(req.params.productId)
+      })
+      .then(reviewWithProductID => {
+        return review.setUser(req.params.user.id)
+      })
+      .then(review => res.status(201).json(review))
+      .catch(next)
+    })
   .get('/:id', (req, res, next) => res.send(req.review))
   .put('/:id/product/:productId/', (req, res, next) => {
     req.review.setProduct(req.params.productId)
@@ -48,3 +57,18 @@ module.exports = require('express').Router()
     req.review.update({isActive: false})
     .then(res.sendStatus(204))
   })
+
+
+//   routerOtter.get('/:otterId/addFamily/:familyId', (req, res, next) => {
+//   let otter;
+//   Otter.findById(req.params.otterId)
+//     .then((foundOtter) => {
+//       otter = foundOtter;
+//       return Family.findById(req.params.familyId);
+//     })
+//     .then((foundFamily) => {
+//       otter.setFamily(foundFamily);
+//       res.sendStatus(200);
+//     })
+//     .catch(next);
+// });
