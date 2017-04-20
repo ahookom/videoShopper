aea14f75397af252a2360bfd315ba77ba688e0a8
Andrew Hookom <hookom@gmail.com>
2017-04-20 17:43:36 -0400

    updating product model and route

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -6,10 +6,10 @@ export default (props) => {
       <div className="thumbnail">
           <img src="http://placehold.it/320x150" alt="" />
           <div className="caption">
-              <h4 className="pull-right">$64.99</h4>
-              <h4><a href="#">Second Product</a>
+              <h4 className="pull-right">{`$${props.price}`}</h4>
+              <h4><a href="#">{props.name}</a>
               </h4>
-              <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>{props.description}</p>
           </div>
           <div className="ratings">
               <p className="pull-right">12 reviews</p>
