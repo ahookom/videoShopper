5bbb33899fed2723f6bfdf6f71e58d12e2dd9268
mandi-m <mmeidlinger@yahoo.com>
2017-04-20 12:51:31 -0400

    finished review routes resolved #26

--- server/reviews.js
+++ server/reviews.js
@@ -17,58 +17,34 @@ module.exports = require('express').Router()
       })
       .catch(next)
   })
-  .get('/',
-    (req, res, next) =>
-      Review.findAll()
-        .then(allReviews => res.json(allReviews))
-        .catch(next))
-  .post('/product/:productId/',
-    mustBeLoggedIn,
-    (req, res, next) => {
-      //console.log('made it to reviews post with body of ', req.body)
-      let review
-      Review.create(req.body)
-      .then(createdReview => {
-        review = createdReview
-        return review.setProduct(req.params.productId)
-      })
-      .then(reviewWithProductID => {
-        return review.setUser(req.params.user.id)
-      })
-      .then(review => res.status(201).json(review))
-      .catch(next)
-    })
+  .get('/', (req, res, next) => Review.findAll()
+    .then(allReviews => res.json(allReviews))
+    .catch(next))
   .get('/:id', (req, res, next) => res.send(req.review))
-  .put('/:id/product/:productId/', (req, res, next) => {
-    req.review.setProduct(req.params.productId)
-    .then(review => {
-      res.json(review)
+  .post('/product/:productId/', mustBeLoggedIn, (req, res, next) => {
+    let review
+    Review.create(req.body)
+    .then(createdReview => {
+      review = createdReview
+      return review.setProduct(req.params.productId)
     })
+    .then(reviewWithProductID => {
+      return review.setUser(req.user.id)
+    })
+    .then(review => res.status(201).json(review))
     .catch(next)
   })
-  .put('/:id/user/:userId/', (req, res, next) => {
-    req.review.setUser(req.params.userId)
-    .then(review => {
-      res.json(review)
+  .put('/:id/product/:productId', mustBeAdminOrSelf, (req, res, next) => {
+    const {title, text, stars, isActive} = req.body
+    Review.findById(req.params.id)
+    .then(foundReview => {
+      return foundReview.updateAttributes({title, text, stars, isActive})
     })
+    .then(updatedReview => updatedReview.setProduct(req.params.productId))
+    .then(updatedProductId => res.status(201).json(updatedProductId))
     .catch(next)
   })
   .delete('/:id', (req, res, next) => {
     req.review.update({isActive: false})
     .then(res.sendStatus(204))
   })
-
-
-//   routerOtter.get('/:otterId/addFamily/:familyId', (req, res, next) => {
-//   let otter;
-//   Otter.findById(req.params.otterId)
-//     .then((foundOtter) => {
-//       otter = foundOtter;
-//       return Family.findById(req.params.familyId);
-//     })
-//     .then((foundFamily) => {
-//       otter.setFamily(foundFamily);
-//       res.sendStatus(200);
-//     })
-//     .catch(next);
-// });
