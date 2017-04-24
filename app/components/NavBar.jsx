763a5924f32c5678aca1273b782e1ea6918192d5
Andrew Hookom <hookom@gmail.com>
2017-04-24 11:58:42 -0400

    added cart to navbar, fixed product model error

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = (props) => {
  return (
 <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div className="container">
-           
+
             <div className="navbar-header">
                 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span className="sr-only">Toggle navigation</span>
@@ -19,28 +19,31 @@ const NavBar = (props) => {
                 </button>
                 <Link className="navbar-brand" to="/">Video Shopper</Link>
             </div>
-          
+
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul className="nav navbar-nav">
                     <li>
                         <Link to='/products'>Products</Link>
                     </li>
                     <li>
+                        <Link to='/cart'>{`Cart ${Object.keys(window.localStorage).length ? '('+Object.keys(window.localStorage).length+')' : ''}`}</Link>
+                    </li>
+                    <li>
                         <LoginBoxes />
                     </li>
                 </ul>
 
-                
-            </div>     
+            </div>
+
         </div>
-        
+
     </nav>
- );
-};
+ )
+}
 
 // ------------- Container
 const mapStateToProps = null;
 
 const mapDispatchToProps = null;
 
-export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
